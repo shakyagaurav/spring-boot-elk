66eb9999ee90da7a447918acb2e18b4427f12432
Gaurav Shakya <gauravshakya71@gmail.com>
2024-09-25 12:43:38 +0530

    ELK metricbeat and heartbeat

--- src/main/java/com/elk/controller/LogController.java
+++ src/main/java/com/elk/controller/LogController.java
@@ -1,6 +1,5 @@
 package com.elk.controller;
 
-import com.elk.publisher.LogsPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +9,6 @@ import java.util.Date;
 @Slf4j
 @RestController
 public class LogController {
-
-    private final LogsPublisher logsPublisher;
-
-    public LogController(LogsPublisher logsPublisher) {
-        this.logsPublisher = logsPublisher;
-    }
 
     @GetMapping("/log")
     public String logMessage() {
